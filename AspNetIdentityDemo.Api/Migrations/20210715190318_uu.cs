d3d4e2b2878413bf11642653ca631f50e284706c
Yassin <rebai.yassin23.yr@gmail.com>
2021-07-15 22:28:34 +0100

    first commit

---
+++ AspNetIdentityDemo.Api/Migrations/20210715190318_uu.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetIdentityDemo.Api.Migrations
+{
+    public partial class uu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ AspNetIdentityDemo.Api/Services/IUserService.cs
@@ -0,0 +1,236 @@
+﻿using AspNetIdentityDemo.Api.Models;
+using AspNetIdentityDemo.Api.Utils;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetIdentityDemo.Api.Services
+{
+    public interface IUserService
+    {
+
+        Task<UserManagerResponse> RegisterUserAsync(RegisterViewModel model);
+
+        Task<UserManagerResponse> LoginUserAsync(LoginViewModel model);
+
+        Task<UserManagerResponse> ConfirmEmailAsync(string userId, string token);
+
+        Task<UserManagerResponse> ForgetPasswordAsync(string email);
+
+        Task<UserManagerResponse> ResetPasswordAsync(ResetPasswordViewModel model);
+    }
+
+    public class UserService : IUserService
+    {
+
+        private UserManager<IdentityUser> _userManger;
+        private IConfiguration _configuration;
+        private IMailService _mailService; 
+        public UserService(UserManager<IdentityUser> userManager, IConfiguration configuration, IMailService mailService) 
+        {
+            _userManger = userManager;
+            _configuration = configuration;
+            _mailService = mailService; 
+        }
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterViewModel model)
+        {
+            if (model == null)
+                throw new NullReferenceException("Reigster Model is null");
+
+            if (model.Password != model.ConfirmPassword)
+                return new UserManagerResponse
+                {
+                    Message = "Confirm password doesn't match the password",
+                    IsSuccess = false,
+                };
+
+            var identityUser = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+            };
+
+            var result = await _userManger.CreateAsync(identityUser, model.Password);
+
+            if(result.Succeeded)
+            {
+                // To verify email, generate token, send token and userId in a link, when clicked it execute a http req
+                var confirmEmailToken = await _userManger.GenerateEmailConfirmationTokenAsync(identityUser);
+
+                var encodedEmailToken = Encoding.UTF8.GetBytes(confirmEmailToken);  //{ because the token contains
+                var validEmailToken = WebEncoders.Base64UrlEncode(encodedEmailToken);// special chars }
+
+                string url = $"{_configuration["AppUrl"]}/api/auth/confirmemail?userid={identityUser.Id}&token={validEmailToken}";
+
+                /*await _mailService.SendEmailAsync(identityUser.Email, "Confirm your email", $"<h1>Welcome to Auth Demo</h1>" +
+                    $"<p>Please confirm your email by <a href='{url}'>Clicking here</a></p>");*/ //Configure SendGrid first
+
+
+                return new UserManagerResponse
+                {
+                    Message = "User created successfully!",
+                    IsSuccess = true,
+                };
+            }
+
+            return new UserManagerResponse
+            {
+                Message = "User did not create",
+                IsSuccess = false,
+                Errors = result.Errors.Select(e => e.Description)
+            };
+
+        }
+
+        public async Task<UserManagerResponse> LoginUserAsync(LoginViewModel model)
+        {
+            var user = await _userManger.FindByEmailAsync(model.Email);
+
+            if(user == null)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "There is no user with that Email address",
+                    IsSuccess = false,
+                };
+            }
+
+            var result = await _userManger.CheckPasswordAsync(user, model.Password);
+
+            if(!result)
+                return new UserManagerResponse
+                {
+                    Message = "Invalid password",
+                    IsSuccess = false,
+                };
+
+            // What inside the token
+            var claims = new[]
+            {
+                new Claim("Email", model.Email), // custom claim
+                new Claim(ClaimTypes.NameIdentifier, user.Id), // predefined claim
+            };
+
+            // Generate token
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AuthSettings:Key"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["AuthSettings:Issuer"],
+                audience: _configuration["AuthSettings:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256));
+
+            string tokenAsString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new UserManagerResponse
+            {
+                Message = tokenAsString,
+                IsSuccess = true,
+                ExpireDate = token.ValidTo
+            };
+        }
+
+        public async Task<UserManagerResponse> ConfirmEmailAsync(string userId, string token)
+        {
+            var user = await _userManger.FindByIdAsync(userId);
+            if (user == null)
+                return new UserManagerResponse
+                {
+                    IsSuccess = false,
+                    Message = "User not found"
+                };
+
+            var decodedToken = WebEncoders.Base64UrlDecode(token);
+            string normalToken = Encoding.UTF8.GetString(decodedToken);
+
+            var result = await _userManger.ConfirmEmailAsync(user, normalToken);
+
+            if (result.Succeeded)
+                return new UserManagerResponse
+                {
+                    Message = "Email confirmed successfully!",
+                    IsSuccess = true,
+                };
+
+            return new UserManagerResponse
+            {
+                IsSuccess = false,
+                Message = "Email did not confirm",
+                Errors = result.Errors.Select(e => e.Description)
+            };
+        }
+
+        public async Task<UserManagerResponse> ForgetPasswordAsync(string email)
+        {
+            var user = await _userManger.FindByEmailAsync(email);
+            if (user == null)
+                return new UserManagerResponse
+                {
+                    IsSuccess = false,
+                    Message = "No user associated with email",
+                };
+
+            var token = await _userManger.GeneratePasswordResetTokenAsync(user);
+            var encodedToken = Encoding.UTF8.GetBytes(token);
+            var validToken = WebEncoders.Base64UrlEncode(encodedToken);
+
+            string url = $"{_configuration["AppUrl"]}/ResetPassword?email={email}&token={validToken}";
+
+            await _mailService.SendEmailAsync(email, "Reset Password", "<h1>Follow the instructions to reset your password</h1>" +
+                $"<p>To reset your password <a href='{url}'>Click here</a></p>");
+
+            return new UserManagerResponse
+            {
+                IsSuccess = true,
+                Message = "Reset password URL has been sent to the email successfully!"
+            };
+        }
+
+        public async Task<UserManagerResponse> ResetPasswordAsync(ResetPasswordViewModel model)
+        {
+            var user = await _userManger.FindByEmailAsync(model.Email);
+            if (user == null)
+                return new UserManagerResponse
+                {
+                    IsSuccess = false,
+                    Message = "No user associated with email",
+                };
+
+            if(model.NewPassword != model.ConfirmPassword)
+                return new UserManagerResponse
+                {
+                    IsSuccess = false,
+                    Message = "Password doesn't match its confirmation",
+                };
+
+            var decodedToken = WebEncoders.Base64UrlDecode(model.Token);
+            string normalToken = Encoding.UTF8.GetString(decodedToken);
+
+            var result = await _userManger.ResetPasswordAsync(user, normalToken, model.NewPassword);
+
+            if (result.Succeeded)
+                return new UserManagerResponse
+                {
+                    Message = "Password has been reset successfully!",
+                    IsSuccess = true,
+                };
+
+            return new UserManagerResponse
+            {
+                Message = "Something went wrong",
+                IsSuccess = false,
+                Errors = result.Errors.Select(e => e.Description),
+            };
+        }
+    }
+}
